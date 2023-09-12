7d74b1dee570bd5e892acbc3e0e843a24ea0f220
Senthil Rajendran <rajsenthil@yahoo.com>
2023-09-12 10:21:20 -0700

    validation tested

--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.19
 require (
 	github.com/iancoleman/orderedmap v0.3.0 // indirect
 	github.com/invopop/jsonschema v0.8.0 // indirect
+	github.com/qri-io/jsonpointer v0.1.1 // indirect
+	github.com/qri-io/jsonschema v0.2.1 // indirect
 	github.com/santhosh-tekuri/jsonschema v1.2.4 // indirect
 )
--- main.go
+++ main.go
@@ -1,23 +1,49 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 
-	"github.com/invopop/jsonschema"
+	"github.com/qri-io/jsonschema"
 )
 
-type Product struct {
+type product struct {
 	ProductName string `json:"productName" jsonschema:"required"`
 	UpcCode     string `json:"upcCode" jsonschema:"required,minLength=12,maxLength=12,description=Upc code,title=Upc Code,example=123456789012"`
 	Attr01      string `json:"attr01"  jsonschema:"required"`
 }
 
 func main() {
-	s := jsonschema.Reflect(&Product{})
-	data, err := json.MarshalIndent(s, "", "  ")
+	ctx := context.Background()
+	sch, err := os.ReadFile("schema/product-attr-validation-schema.json")
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err)
 	}
-	fmt.Println(string(data))
+
+	fmt.Printf("%v\n", string(sch))
+
+	product01, err := os.ReadFile("schema/product01.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%v\n", string(product01))
+
+	rs := &jsonschema.Schema{}
+	if err := json.Unmarshal(sch, rs); err != nil {
+		panic("unmarshal schema: " + err.Error())
+	}
+
+	errs, err := rs.ValidateBytes(ctx, product01)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(errs) > 0 {
+		fmt.Println(errs[0].Error())
+	}
+
 }
