9dafe877f8abcd5fe74e44eba3532bef7bb3e1bc
Senthil Rajendran <rajsenthil@yahoo.com>
2023-09-11 18:05:09 -0700

    schema generation

--- main.go
+++ main.go
@@ -3,38 +3,21 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 
-	"github.com/santhosh-tekuri/jsonschema"
+	"github.com/invopop/jsonschema"
 )
 
 type Product struct {
-	ProductName string `json:"productName"`
-	UpcCode     string `json:"upcCode"`
-	Attr01      string `json:"attr01"`
+	ProductName string `json:"productName" jsonschema:"required"`
+	UpcCode     string `json:"upcCode" jsonschema:"required,minLength=12,maxLength=12,description=Upc code,title=Upc Code,example=123456789012"`
+	Attr01      string `json:"attr01"  jsonschema:"required"`
 }
 
 func main() {
-	sch, err := jsonschema.Compile("schema/product-attr-validation-schema.json")
+	s := jsonschema.Reflect(&Product{})
+	data, err := json.MarshalIndent(s, "", "  ")
 	if err != nil {
-		log.Fatalf("%#v", err)
+		panic(err.Error())
 	}
-
-	data, err := ioutil.ReadFile("schema/product01.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var prod = new(Product)
-
-	if err := json.Unmarshal(data, &prod); err != nil {
-		log.Fatal(err)
-	}
-
-	log.Printf("Prod: %v", prod)
-	fmt.Printf("Prod: %v", prod)
-	if err = sch.ValidateInterface(prod); err != nil {
-		log.Fatalf("Error %v", err)
-	}
+	fmt.Println(string(data))
 }
